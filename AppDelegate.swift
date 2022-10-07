6291a1c0a2171b8c45b62b962849470386981f40
MyHa <hantm.nta@gmail.com>
2022-10-07 15:22:26 +0700

    01

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate,MessagingDelegate,UNUserNo
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // 01
         return true
     }
 }
