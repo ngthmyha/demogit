ab08064cc4635b213cd8a1a43b1ab77fb535bb50
MyHa <hantm.nta@gmail.com>
2022-10-07 15:17:15 +0700

    First commit

---
+++ AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  KaraNew
+//
+//  Created by Akshay Purohit on 4/22/21.
+//
+
+import UIKit
+import Foundation
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate,MessagingDelegate,UNUserNotificationCenterDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+        return true
+    }
+}
